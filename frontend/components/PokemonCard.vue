e7c90d6749e18c45d92efe35ff12e01041f439ab
Davi Rangel <davirangelcamara@hotmail.com>
2024-10-07 13:41:05 -0300

    Creating the favorite pokemon option

--- frontend/components/PokemonCard.vue
+++ frontend/components/PokemonCard.vue
@@ -15,19 +15,40 @@
         {{ capitalizeFirstLetter(pokemon.name) }}
       </div>
     </div>
+    <v-btn
+      icon
+      variant="text"
+      color="#ff5350"
+      style="width: 15px; height: 15px"
+      :class="{ favorited: isFavorited }"
+      @click.stop="toggleFavorite"
+    >
+      <v-icon style="width: 15px; height: 15px">{{
+        isFavorited ? "mdi-heart" : "mdi-heart-outline"
+      }}</v-icon>
+    </v-btn>
   </div>
 </template>
 
-<script lang="ts">import { defineComponent } from "vue";
+<script lang="ts">
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'PokemonCard',
+  name: "PokemonCard",
   props: {
     pokemon: {
       type: Object,
       required: true,
-      default: () => ({})
-    }
+      default: () => ({}),
+    },
+  },
+  data() {
+    return {
+      isFavorited: false,
+    };
+  },
+  created() {
+    this.checkFavoriteStatus();
   },
   methods: {
     getPokemonImage(name: string) {
@@ -48,6 +69,29 @@ export default defineComponent({
     formatId(id: number): string {
       return id.toString().padStart(4, "0");
     },
-  }
-})
+    checkFavoriteStatus() {
+      const favorites = JSON.parse(
+        localStorage.getItem("pokemonFavorites") || "[]"
+      );
+      this.isFavorited = favorites.includes(this.pokemon.name);
+    },
+    toggleFavorite() {
+      const favorites = JSON.parse(
+        localStorage.getItem("pokemonFavorites") || "[]"
+      );
+      if (this.isFavorited) {
+        // Remove from favorites
+        const index = favorites.indexOf(this.pokemon.name);
+        if (index > -1) {
+          favorites.splice(index, 1);
+        }
+      } else {
+        // Add to favorites
+        favorites.push(this.pokemon.name);
+      }
+      localStorage.setItem("pokemonFavorites", JSON.stringify(favorites));
+      this.isFavorited = !this.isFavorited;
+    },
+  },
+});
 </script>
